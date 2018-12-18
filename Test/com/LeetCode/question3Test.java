85759ff8fff04613d98b11833587edf646a96493
0APPTI0 <2458530429@qq.com>
2018-12-18 19:56:35 +0800

    增加了习题三

---
+++ Test/com/LeetCode/question3Test.java
@@ -0,0 +1,12 @@
+package com.LeetCode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class question3Test {
+
+    @Test
+    void allUnique() {
+    }
+}
