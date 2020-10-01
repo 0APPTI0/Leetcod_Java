ab5390ee911400feaa4fc308823957de07312e50
张士煜 <171250525@smail.nju.edu.cn>
2020-10-01 20:52:19 +0800

    10-1

---
+++ EveryDayPractise/_53最大子序和/Solution.java
@@ -0,0 +1,13 @@
+package _53最大子序和;
+
+// 参照官方动态规划题解
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+}
