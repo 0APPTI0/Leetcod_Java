8093bd403f3a5f398ec341bb76d30407e0c53e94
张士煜 <171250525@smail.nju.edu.cn>
2020-10-03 10:43:49 +0800

    10-3

--- EveryDayPractise/_1两数之和/Solution.java
+++ EveryDayPractise/_1两数之和/Solution.java
@@ -1,14 +1,17 @@
 package _1两数之和;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] == target - nums[i]) {
-                    return new int[] { i, j };
-                }
+        Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
             }
+            hashtable.put(nums[i], i);
         }
-        throw new IllegalArgumentException("No two sum solution");
+        return new int[0];
     }
 }
